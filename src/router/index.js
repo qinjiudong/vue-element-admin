d4637b7977ac2d5a272fcaff9a14328b80ac5e10
qjd <qjd@qq.com>
2020-04-25 10:41:19 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,13 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
-
-    {
-        path: '/',
-        component: () => import('../views/Login.vue')
-    }
-]
+const routes = [{
+    path: '/login',
+    component: () => import('../views/Login.vue')
+}, {
+    path: '/',
+    component: () => import('../views/Home.vue')
+}]
 
 const router = new VueRouter({
     mode: 'history',
