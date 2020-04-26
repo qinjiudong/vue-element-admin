bcb148c9117b3f314c54e55dfb079ef8fd1773e2
jd <qjd@163.cn>
2020-04-26 12:19:54 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,25 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [{
+    path: '*',
+    redirect: '/404',
+}, {
     path: '/login',
     component: () => import('../views/Login.vue')
 }, {
     path: '/',
-    component: () => import('../views/Home.vue')
+    redirect: '/index',
+    component: () => import('../views/Home.vue'),
+    children: [{
+        path: '/404',
+        component: () => import('../views/404.vue')
+    }, {
+        path: '/index',
+        component: () => import('../views/Index.vue')
+    }, {
+        path: '/table1',
+        component: () => import('../views/Table1.vue')
+    }, ]
 }]
 
 const router = new VueRouter({
