48ee91aa0d3036fd8f9fe77f2aeed1bea25b1423
jd <qjd@163.cn>
2020-04-29 12:21:53 +0800

    1

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    productionSourceMap: false,
-    configureWebpack: {
-        externals: {
-            'vue': 'Vue',
-            'element-ui': 'ELEMENT'
-        }
-    }
-}
